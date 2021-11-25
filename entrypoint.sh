278074391be68494191843758301ae3db7272c39
benjfield <benjfield@hotmail.com>
2021-11-25 13:09:15 +0000

    Allowed bidirectional movements

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,64 @@
 
 set -e
 
-if [ -z "$AWS_S3_BUCKET" ]; then
-  echo "AWS_S3_BUCKET is not set. Quitting."
-  exit 1
+if [ -z "$USE_AWS_FOR_DESTINATION" ]; then
+  USE_AWS_FOR_DESTINATION=true
+else
+  USE_AWS_FOR_DESTINATION=`echo "${USE_AWS_FOR_DESTINATION}" | tr '[:upper:]' '[:lower:]'`
+fi
+
+if [ "$USE_AWS_FOR_DESTINATION"=true ]; then
+  if [ -z "$AWS_DESTINATION_S3_BUCKET" ]; then
+    echo "AWS_S3_BUCKET is not set. Quitting."
+    exit 1
+  fi
+
+  # Default to us-east-1 if AWS_REGION not set.
+  if [ -z "$AWS_DESTINATION_REGION" ]; then
+    AWS_REGION="us-east-1"
+    AWS_DESTINATION_REGION_STRING="--region ${AWS_REGION} "
+  fi
+
+  if ! [ -z "$DESTINATION_DIR" ]; then
+    DESTINATION_DIR="/${DESTINATION_DIR}"
+  fi
+
+  DESTINATION_STRING="s3://${AWS_DESTINATION_S3_BUCKET}${DESTINATION_DIR}"
+else
+  if [ -z "$DESTINATION_DIR" ]; then
+    DESTINATION_STRING="."
+  else
+    DESTINATION_STRING=$DESTINATION_DIR
+  fi
+fi
+
+if [ -z "$USE_AWS_FOR_SOURCE" ]; then
+  USE_AWS_FOR_SOURCE=true
+else
+  USE_AWS_FOR_SOURCE=`echo "${USE_AWS_FOR_SOURCE}" | tr '[:upper:]' '[:lower:]'`
+fi
+
+if [ "$USE_AWS_FOR_SOURCE"=true ]; then
+  if [ -z "$AWS_SOURCE_S3_BUCKET" ]; then
+    echo "AWS_S3_BUCKET is not set. Quitting."
+    exit 1
+  fi
+
+  if [ -z "$AWS_SOURCE_REGION" ]; then
+    AWS_SOURCE_REGION_STRING="--source-region ${AWS_REGION} "
+  fi
+
+  if ! [ -z "$SOURCE_DIR" ]; then
+    SOURCE_DIR="/${SOURCE_DIR}"
+  fi
+
+  SOURCE_STRING="s3://${AWS_SOURCE_S3_BUCKET}${SOURCE_DIR}"
+else
+  if [ -z "$SOURCE_DIR" ]; then
+    SOURCE_STRING="."
+  else
+    SOURCE_STRING=$SOURCE_DIR
+  fi
 fi
 
 if [ -z "$AWS_ACCESS_KEY_ID" ]; then
@@ -17,39 +72,26 @@ if [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
   exit 1
 fi
 
-# Default to us-east-1 if AWS_REGION not set.
-if [ -z "$AWS_REGION" ]; then
-  AWS_REGION="us-east-1"
-fi
-
-# Override default AWS endpoint if user sets AWS_S3_ENDPOINT.
-if [ -n "$AWS_S3_ENDPOINT" ]; then
-  ENDPOINT_APPEND="--endpoint-url $AWS_S3_ENDPOINT"
-fi
-
 # Create a dedicated profile for this action to avoid conflicts
 # with past/future actions.
 # https://github.com/jakejarvis/s3-sync-action/issues/1
-aws configure --profile s3-sync-action <<-EOF > /dev/null 2>&1
+aws configure --profile s3-sync-action-bidirectional <<-EOF > /dev/null 2>&1
 ${AWS_ACCESS_KEY_ID}
 ${AWS_SECRET_ACCESS_KEY}
-${AWS_REGION}
 text
 EOF
 
 # Sync using our dedicated profile and suppress verbose messages.
 # All other flags are optional via the `args:` directive.
-sh -c "aws s3 sync ${SOURCE_DIR:-.} s3://${AWS_S3_BUCKET}/${DEST_DIR} \
-              --profile s3-sync-action \
-              --no-progress \
-              ${ENDPOINT_APPEND} $*"
+sh -c "aws s3 sync ${SOURCE_STRING} ${DESTINATION_STRING} ${AWS_SOURCE_REGION_STRING}${AWS_DESTINATION_REGION_STRING}\
+              --profile s3-sync-action-bidirectional \
+              --no-progress $*"
 
 # Clear out credentials after we're done.
 # We need to re-run `aws configure` with bogus input instead of
 # deleting ~/.aws in case there are other credentials living there.
 # https://forums.aws.amazon.com/thread.jspa?threadID=148833
-aws configure --profile s3-sync-action <<-EOF > /dev/null 2>&1
-null
+aws configure --profile s3-sync-action-bidirectional <<-EOF > /dev/null 2>&1
 null
 null
 text
