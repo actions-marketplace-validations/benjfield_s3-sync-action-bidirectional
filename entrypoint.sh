ed2aef976a259b4e393f075ca05b2061e2ff4fde
benjfield <benjfield@hotmail.com>
2021-11-25 14:51:28 +0000

    further debug statement

--- entrypoint.sh
+++ entrypoint.sh
@@ -89,6 +89,10 @@ echo "${DESTINATION_STRING}" | sed 's/./& /g'
 echo "${AWS_SOURCE_REGION_STRING}"
 echo "${AWS_DESTINATION_REGION_STRING}"
 
+echo "aws s3 sync ${SOURCE_STRING} ${DESTINATION_STRING} ${AWS_SOURCE_REGION_STRING}${AWS_DESTINATION_REGION_STRING}\
+              --profile s3-sync-action-bidirectional \
+              --no-progress $*"
+
 # Sync using our dedicated profile and suppress verbose messages.
 # All other flags are optional via the `args:` directive.
 sh -c "aws s3 sync ${SOURCE_STRING} ${DESTINATION_STRING} ${AWS_SOURCE_REGION_STRING}${AWS_DESTINATION_REGION_STRING}\
